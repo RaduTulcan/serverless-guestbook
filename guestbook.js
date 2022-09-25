9a3566b1fa1793fa89f8526ba6d3db3c465b44a2
Radu Florin Tulcan <radu.tulcan@yahoo.com>
2022-09-25 16:13:17 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://eu-de.functions.appdomain.cloud/api/v1/web/organizationMine_sentiment-analyzer/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
